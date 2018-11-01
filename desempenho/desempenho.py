c2b44351cad893b11ff6c0935d62113be40182d6
Vitor Gomes <vconrado@gmail.com>
2018-11-01 16:02:20 -0300

    implementando execucao em threads

--- desempenho/desempenho.py
+++ desempenho/desempenho.py
@@ -8,6 +8,8 @@ import geopandas as gpd
 #import matplotlib.pyplot as plt
 from time import gmtime, strftime
 import os
+import threading
+import time
 
 dc = datacube.Datacube()
 
@@ -36,11 +38,8 @@ def get_data(product, x, y, time):
                         resolution = product.grid_spec.resolution)
 
 
-
-
-
 def test_func(test_name, product_name, x_steps, y_steps, ts=False, time_lst=None, maximo=None):
-    product = dc.index.products.get_by_name(product_name)
+   product = dc.index.products.get_by_name(product_name)
     
     print("Running {}:{}  ".format(test_name, product_name))
     
@@ -119,6 +118,99 @@ def test_func(test_name, product_name, x_steps, y_steps, ts=False, time_lst=None
                 
     df = pd.DataFrame(d)
     return gpd.GeoDataFrame(df, geometry='geom', crs={'init': product.grid_spec.crs.crs_str})
+
+
+def the_thread(test_name, product, x, dx, y, dy, time_lst, ts, data, index):
+    if ts is True:
+        geom = geometry.Point([x+dx/2, y+dx/2])
+    else:
+        geom = geometry.Polygon([[x, y],
+                             [x+dx, y],
+                             [x+dx, y+dy],
+                             [x, y+dy],
+                             [x, y]])
+    start_time = time.time()
+    if ts is True:
+        data = get_data(product, x+dx/2, y+dx/2, time_lst)
+    else:
+        data = get_data(product, (x, x+dx), (y, y+dy), time_lst)
+    dif_time = (time.time() - start_time)
+
+    data_loaded_B=data.sizes["x"] * data.sizes["y"] * data.sizes["time"] * cell_size
+    MB_s = (data_loaded_B/(1025*1024))/dif_time
+
+    d = {        
+        "prod": product.name,
+        "count": count, 
+        "x0": x, 
+        "xf": x+dx, 
+        "y0": y, 
+        "yf": y+dy, 
+        "x_size": data.sizes["x"], 
+        "y_size":data.sizes["y"], 
+        "time_size": data.sizes["time"],
+        "data_loaded_B":data_loaded_B,
+        "MB_s":MB_s,
+        "dif_time": dif_time,
+        "geom": geom
+        })
+    result[index] = d
+    print("Thread {}.{}#{} finalizada".format(test_name, product.name, index))
+        
+
+def p_test_func(threads, test_name, product_name, x_steps, y_steps, ts=False, time_lst=None, maximo=None):
+    product = dc.index.products.get_by_name(product_name)
+    
+    print("Running {}:{}  ".format(test_name, product_name))
+    
+    # calc cell size
+    cell_size = get_prod_cell_size(product)
+        
+    ds_lst = dc.find_datasets(product=product.name)
+    product_bbox = datacube.api.core.get_bounds(ds_lst, product.grid_spec.crs)
+    
+    sp = loads(product_bbox.wkt)
+    xs, ys = sp.exterior.xy
+    
+    min_x = min(xs)
+    min_y = min(ys)
+    max_x = max(xs)
+    max_y = max(ys)
+
+    dx = (max_x - min_x)/(x_steps)
+    dy = (max_y - min_y)/(y_steps)
+    
+    x0 = min_x
+    y0 = min_y
+    count = 0
+    result = []
+
+    if maximo is None:
+        maximo = float("inf")
+
+    threads = []    
+    for i in range(0,x_steps):
+        x = x0 + i*dx
+        for j in range(0, y_steps):
+            y = y0 + j*dy
+            if count < maximo:
+                args = (test_name, product, x, dx, y, dy, time_lst, ts, result, count)
+                t = threading.Thread(target=the_thread,args=*args)
+                threads.append(t)
+            
+            count=count+1
+    
+    for i in range(0, len(threads)):
+        threads[i].start();
+    
+    print("Aguardando finalizacao das threads");
+    for i in range(0, len(threads)):
+        threads[i].join();
+    
+    df = pd.DataFrame(result)
+    return gpd.GeoDataFrame(df, geometry='geom', crs={'init': product.grid_spec.crs.crs_str})
+
+
 
 def save_gdf(gdf, product_name, rodada, subdir, columns=None):
     base_path = "./dados"
